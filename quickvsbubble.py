fee0589de760a10d1a1f9969d5112b6bcf7c5f83
Evita2006 <evamarti@cabanell.es>
2025-09-16 09:47:08 +0200

    Add files via upload

---
+++ quickvsbubble.py
@@ -0,0 +1,24 @@
+import time, random
+
+def bubble_sort(array):
+    n = len(array)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+                
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+    pivot = array[len(array) // 2]
+    left = [x for x in array if x < pivot]
+    middle = [x for x in array if x == pivot]
+    right = [x for x in array if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+data = [random.randint(0, 10000) for _ in range(2000)]
+for algo in [bubble_sort, quick_sort]:
+    start = time.time()
+    algo(data.copy())
+    print(algo.__name__, time.time() - start) 
+    
